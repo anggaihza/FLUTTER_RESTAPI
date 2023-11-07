fb31cd0ad1c3148b1cb3953830dd2e15d80a517a
anggaihza <angga.asep98@gmail.com>
2023-11-07 07:37:38 +0700

    first commit

---
+++ lib/utils/snack_message.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:consune_rest_api/styles/colors.dart';
+
+void showMessage({String? message, BuildContext? context}) {
+  ScaffoldMessenger.of(context!).showSnackBar(SnackBar(
+      content: Text(
+        message!,
+        style: TextStyle(color: white),
+      ),
+      backgroundColor: primaryColor));
+}
